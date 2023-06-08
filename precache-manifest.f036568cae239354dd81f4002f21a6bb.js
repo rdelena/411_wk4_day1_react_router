167823900bb241e6e46c52e0cc64e88db1c8100c
rdelena <rodnydelena@gmail.com>
2023-06-07 19:22:03 -0500

    Updates

---
+++ precache-manifest.f036568cae239354dd81f4002f21a6bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "951a2cf78c220a6ec97012974f1e3b31",
+    "url": "/411_wk4_day1_react_router/index.html"
+  },
+  {
+    "revision": "05aa06abf4bdbec3e64e",
+    "url": "/411_wk4_day1_react_router/static/css/main.05ad2c06.chunk.css"
+  },
+  {
+    "revision": "57e9e63c4527a563c9a6",
+    "url": "/411_wk4_day1_react_router/static/js/2.cd7de125.chunk.js"
+  },
+  {
+    "revision": "05aa06abf4bdbec3e64e",
+    "url": "/411_wk4_day1_react_router/static/js/main.1835b369.chunk.js"
+  },
+  {
+    "revision": "0e9dcfd1a95c89889604",
+    "url": "/411_wk4_day1_react_router/static/js/runtime~main.0c971c59.js"
+  }
+]);
